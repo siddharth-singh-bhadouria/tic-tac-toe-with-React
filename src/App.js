c427e8591e0f438dc903cbaa36a6a4c7533a8ff8
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-11 20:21:22 +0530

    Setting up block and squares

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Square from './Components/Square'
+import { useState } from 'react'
 
-function App() {
+export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
   return (
-    <div>
-
-    </div>
-  );
+    <>
+      <div className="board-row">
+        <Square value={square[0]} />
+        <Square value={square[1]} />
+        <Square value={square[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={square[3]} />
+        <Square value={square[4]} />
+        <Square value={square[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={square[6]} />
+        <Square value={square[7]} />
+        <Square value={square[8]} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+
---
+++ src/Components/Square.js
@@ -0,0 +1,18 @@
+
+const Square = ({ value }) => {
+
+    // function handleClick() {
+    //     setValue('X')
+    // }
+
+    return (
+        <button className='square'>{value}</button>
+    )
+}
+
+export default Square
+
+
+
+
+
