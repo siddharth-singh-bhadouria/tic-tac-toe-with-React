1bc3461feb4782e55f00c2b6d4e0784be30ae0a3
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-13 02:18:14 +0530

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Board from './Components/Board';
 import { useState } from 'react'
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0
   const currentSquare = history[currentMove]
   const nextSquares = currentSquare.slice()
 
@@ -13,12 +13,10 @@ export default function Game() {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory)
     setCurrentMove(nextHistory.length - 1)
-    setXIsNext(!xIsNext)
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove)
-    setXIsNext(nextMove % 2 === 0)
   }
 
   const moves = history.map((squares, move) => {
